a3b8c53f9f732a9062e1fe552857356b588d79a3
justins707 <35348272+justins707@users.noreply.github.com>
2026-06-17 15:56:45 -0700

    Update Order_Smoothie.py
    Connection changes due to move from SiS to SniS

--- Order_Smoothie.py
+++ Order_Smoothie.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session 
+# from snowflake.snowpark.context import get_active_session 
 from snowflake.snowpark.functions import col
 #import os
 
@@ -10,7 +10,9 @@ st.write(
   """Choose up to 5 fruits for your custom Smoothie.
   """
 )
-session = get_active_session()
+#  session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 name_for_order = st.text_input('Enter your Name: ')
